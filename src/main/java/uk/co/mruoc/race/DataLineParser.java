9c5b109a4d3dd38a5dede45492dee534ce8a2f9a
Michael Ruocco <michael.ruocco@hotmail.com>
2016-11-27 09:31:38 +0000

    refactors data line parser

--- src/main/java/uk/co/mruoc/race/DataLineParser.java
+++ src/main/java/uk/co/mruoc/race/DataLineParser.java
@@ -5,19 +5,36 @@ import uk.co.mruoc.time.ElapsedTimeConverter;
 
 public class DataLineParser {
 
+    private static final int TIME_INDEX = 0;
+    private static final int CAR_ID_INDEX = 1;
+    private static final int CHECKPOINT_ID_INDEX = 2;
+    private static final int QUERIED_INDEX = 3;
+
     private final ElapsedTimeConverter elapsedTimeConverter = new ElapsedTimeConverter();
 
     public DataLine parse(String input) {
         String[] args = input.split(" ");
-        ElapsedTime time = toTime(args[0]);
-        int carId = Integer.parseInt(args[1]);
-        char checkpointId = args[2].charAt(0);
-        boolean queried = Integer.parseInt(args[3]) > 0;
+        ElapsedTime time = toTime(args[TIME_INDEX]);
+        int carId = toInt(args[CAR_ID_INDEX]);
+        char checkpointId = toChar(args[CHECKPOINT_ID_INDEX]);
+        boolean queried = toBoolean(args[QUERIED_INDEX]);
         return new DataLine(time, carId, checkpointId, queried);
     }
 
     private ElapsedTime toTime(String input) {
         return elapsedTimeConverter.toElapsedTime(input);
+    }
+
+    private int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private char toChar(String input) {
+        return input.charAt(0);
+    }
+
+    private boolean toBoolean(String input) {
+        return toInt(input) != 0;
     }
 
 }
