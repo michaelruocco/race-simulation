fde4fd370bf2be17172ef5953dbdc73809d722ab
Michael Ruocco <michael.ruocco@hotmail.com>
2016-11-28 17:23:36 +0000

    adds coverage for loading file that does not exist

--- src/test/java/uk/co/mruoc/race/FileLoaderTest.java
+++ src/test/java/uk/co/mruoc/race/FileLoaderTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 import uk.co.mruoc.time.ElapsedTimeConverter;
 
 import java.io.File;
+import java.io.UncheckedIOException;
 
+import static com.googlecode.catchexception.apis.BDDCatchException.caughtException;
+import static com.googlecode.catchexception.apis.BDDCatchException.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
 
 public class FileLoaderTest {
 
@@ -33,6 +37,15 @@ public class FileLoaderTest {
         assertThat(carsData.getLineCountForCar(5)).isEqualTo(163);
         assertThat(carsData.getLineCountForCar(6)).isEqualTo(163);
         assertThat(carsData.getLineCountForCar(7)).isEqualTo(163);
+    }
+
+    @Test
+    public void shouldThrowErrorIfFileDoesNotExist() {
+        when(loader).load(new File("nonExistent"));
+
+        then(caughtException())
+                .isInstanceOf(UncheckedIOException.class)
+                .hasMessage("java.io.FileNotFoundException: File 'nonExistent' does not exist");
     }
 
 }
