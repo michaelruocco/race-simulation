3dabe9ebf2b09c9e7f5afee9ac2e156912f2bc53
Michael Ruocco <michael.ruocco@hotmail.com>
2016-12-22 14:21:51 +0000

    adds speed calculator tests

--- src/main/java/uk/co/mruoc/race/model/SpeedCalculator.java
+++ src/main/java/uk/co/mruoc/race/model/SpeedCalculator.java
@@ -3,15 +3,17 @@ package uk.co.mruoc.race.model;
 import uk.co.mruoc.time.ElapsedTime;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
+
+import static java.math.BigDecimal.*;
+import static java.math.MathContext.*;
 
 public class SpeedCalculator {
 
     public static BigDecimal calculate(BigDecimal distance, ElapsedTime time) {
-        BigDecimal totalMillis = BigDecimal.valueOf(time.getTotalMillis());
-        if (totalMillis.equals(BigDecimal.ZERO))
-            return BigDecimal.ZERO;
-        return distance.divide(totalMillis, MathContext.DECIMAL32);
+        BigDecimal totalMillis = valueOf(time.getTotalMillis());
+        if (totalMillis.equals(ZERO))
+            return ZERO;
+        return distance.divide(totalMillis, DECIMAL32);
     }
 
 }
---
+++ src/test/java/uk/co/mruoc/race/SpeedCalculatorTest.java
@@ -0,0 +1,35 @@
+package uk.co.mruoc.race;
+
+import org.junit.Test;
+import uk.co.mruoc.race.model.SpeedCalculator;
+import uk.co.mruoc.time.ElapsedTime;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SpeedCalculatorTest {
+
+    @Test
+    public void shouldCalculateSpeedInMetersPerMillisecond1() {
+        BigDecimal oneHundredMeters = BigDecimal.valueOf(100);
+        ElapsedTime thirtySeconds = new ElapsedTime("00:00:30.000");
+        BigDecimal expectedSpeed = BigDecimal.valueOf(0.003333333);
+
+        BigDecimal speed = SpeedCalculator.calculate(oneHundredMeters, thirtySeconds);
+
+        assertThat(speed).isEqualTo(expectedSpeed);
+    }
+
+    @Test
+    public void shouldCalculateSpeedInMetersPerMillisecond2() {
+        BigDecimal oneHundredMeters = BigDecimal.valueOf(500);
+        ElapsedTime thirtySeconds = new ElapsedTime("00:01:00.000");
+        BigDecimal expectedSpeed = BigDecimal.valueOf(0.008333333);
+
+        BigDecimal speed = SpeedCalculator.calculate(oneHundredMeters, thirtySeconds);
+
+        assertThat(speed).isEqualTo(expectedSpeed);
+    }
+
+}
