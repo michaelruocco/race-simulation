274b4e5586752ce7d683ea5d4b74d05042f60e37
Michael Ruocco <michael.ruocco@hotmail.com>
2017-02-01 07:11:30 +0000

    changes text font in about window

--- src/main/java/uk/co/mruoc/race/gui/AboutPopupWindow.java
+++ src/main/java/uk/co/mruoc/race/gui/AboutPopupWindow.java
@@ -1,6 +1,9 @@
 package uk.co.mruoc.race.gui;
 
 import javax.swing.*;
+import javax.swing.text.EditorKit;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 import java.awt.*;
 
 public class AboutPopupWindow extends PopupWindow {
@@ -14,18 +17,26 @@ public class AboutPopupWindow extends PopupWindow {
 
     private static class AboutPane extends JEditorPane {
 
-        private static final String TEXT = "<p>This race simulation program has been produced by Michael Ruocco.</p>" +
-                "<p>It is a rework of a solution to a computer programming project that was done as part of the CS223 - " +
-                "Introduction to Software Engineering module in the second year of the computer science degree at " +
-                "the University of Warwick.</p>" +
+        private static final String TEXT = "<html><head></head><body><p>This race simulation program has been produced " +
+                "by Michael Ruocco.</p><p>It is a rework of a solution to a computer programming project that was done" +
+                "as part of the CS223 - Introduction to Software Engineering module in the second year of the computer " +
+                "science degree at the University of Warwick.</p>" +
                 "<p>The original attempt was created by group \"Tau 4 Now\" which included the following members:</p>" +
                 "<ul><li>Michael Ruocco</li><li>Edward Steel</li><li>James Gough</li><li>Christopher Dean</li>" +
-                "<li>Philip Ananin</li><li>Mark Flintstone</li></ul>";
+                "<li>Philip Ananin</li><li>Mark Flintstone</li></ul></body></html>";
 
         public AboutPane() {
             setEditable(false);
             setContentType("text/html");
+            setEditorKit(buildEditorKit());
             setText(TEXT);
+        }
+
+        private EditorKit buildEditorKit() {
+            HTMLEditorKit editorKit = new HTMLEditorKit();
+            StyleSheet styleSheet = editorKit.getStyleSheet();
+            styleSheet.addRule("body { font-family:Arial; font-size:small; }");
+            return editorKit;
         }
 
     }
